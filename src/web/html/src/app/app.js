015b3b4447d436401664bbd125ab8e1897e3d932
Neylor Ohmaly <nohros.ceo@gmail.com>
2013-10-14 20:08:39 -0300

    Initial folder structure

---
+++ src/web/html/src/app/app.js
@@ -0,0 +1,22 @@
+angular.module( 'ngBoilerplate', [
+  'templates-app',
+  'templates-common',
+  'ui.router'
+])
+
+.config( function AppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle;
+    }
+  });
+})
+
+;
